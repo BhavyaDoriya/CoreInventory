012b3f0495fbd1f553836b096f73866fa5293460
Bhavya <bhavyadoriya2007@gmail.com>
2026-03-14 12:11:18 +0530

    Added RBAC

--- core_api/stock_ledger/views.py
+++ core_api/stock_ledger/views.py
@@ -1,36 +1,34 @@
-from django.shortcuts import render
-from rest_framework import viewsets, permissions, filters # Added filters
-from django_filters.rest_framework import DjangoFilterBackend # New import
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.decorators import action
-# Create your views here.
-from rest_framework import viewsets, permissions
+
 from .models import Warehouse, Location, Product, StockMovement
 from .serializers import (
     WarehouseSerializer, LocationSerializer, 
     ProductSerializer, StockMovementSerializer
 )
+from .permissions import IsManagerOrReadOnly
 
 class WarehouseViewSet(viewsets.ModelViewSet):
     queryset = Warehouse.objects.all()
     serializer_class = WarehouseSerializer
-    # This ensures only logged-in people can see the data
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsManagerOrReadOnly]
 
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsManagerOrReadOnly]
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsManagerOrReadOnly]
     
     # The Search Engine
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['category'] # Lets frontend filter by Raw Material, etc.
-    search_fields = ['name', 'sku'] # Lets frontend type "Steel" and find it
+    filterset_fields = ['category'] 
+    search_fields = ['name', 'sku'] 
 
 class StockMovementViewSet(viewsets.ModelViewSet):
     queryset = StockMovement.objects.all().order_by('-date')
@@ -44,3 +42,19 @@ class StockMovementViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # ... (Keep Aryan's math logic exactly as it is here!)
         pass
+
+class DashboardViewSet(viewsets.ViewSet):
+    """Feeds the React Dashboard KPI Cards instantly"""
+    permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=False, methods=['get'])
+    def kpis(self, request):
+        total_products = Product.objects.count()
+        low_stock = Product.objects.filter(current_stock__lt=10).count() 
+        total_movements = StockMovement.objects.count()
+        
+        return Response({
+            "total_products": total_products,
+            "low_stock_alerts": low_stock,
+            "total_movements": total_movements
+        })
